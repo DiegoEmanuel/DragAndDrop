fc9dc6e5938702875f5d8eaceb9cfe915f4213fb
Diego Emanuel <diegoabox@gmail.com>
2022-04-08 02:36:53 -0300

    registration screen and initial commit of leads process

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,123 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="lead">
+    <figure class="imagem">
+      <img src="../assets/logo.png" alt="Minha Figura" />
+      <h1 class="display-2">Nova lead</h1>
+    </figure>
+  </div>
+  <div class="container">
+    <div class="form-group col-md-6"></div>
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm">
+        <form @submit.prevent="register">
+          <div class="form-row">
+            <div class="form-group">
+              <aria-label for="name">Nome</aria-label>
+              <input
+                placeholder="Nome"
+                v-model="v$.name.$model"
+                class="form-control"
+                type="text"
+                name="name"
+                id="name"
+              />
+              <span v-if="v$.name.$error" class="text-danger"
+                >Campo é requerido</span
+              >
+            </div>
+          </div>
+          <div class="form-row col-md-3">
+            <div class="form-group">
+              <aria-label for="telefone">Telefone</aria-label>
+              <input
+                placeholder="Telefone"
+                class="form-control"
+                type="telefone"
+                name="telefone"
+                id="telefone"
+                v-model="v$.telefone.$model"
+              />
+            </div>
+          </div>
+          <div class="form-row col-md-3">
+            <div class="form-group">
+              <aria-label for="email">Email</aria-label>
+              <input
+                placeholder="Email"
+                class="form-control"
+                type="email"
+                name="email"
+                id="email"
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+      <div class="col-sm">Uma de três colunas</div>
+      <div class="col-sm">
+        <form>
+          <div class="form-row">
+            <div class="form-group">
+              <aria-label for="email">Email</aria-label>
+              <input
+                placeholder="Email"
+                class="form-control"
+                type="email"
+                name="email"
+                id="email"
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import useVuelidate from "@vuelidate/core";
+import { required } from "@vuelidate/validators";
+
+export default {
+  setup() {
+    return { v$: useVuelidate() };
+  },
+  data() {
+    return {
+      name: "",
+      telefone: "",
+      email: "",
+    };
+  },
+  methods: {},
+  validations() {
+    return {
+      name: { required },
+      telefone: {
+        required,
+      },
+      email: {
+        required,
+      },
+    };
+  },
+  components: {},
+};
+</script>
+<style>
+.lead {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.container {
+  background-color: aqua;
+}
+.imagem {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/views/SingIn.vue
@@ -0,0 +1,160 @@
+<template>
+  <div class="login">
+    <div class="card">
+      <figure class="imagem">
+        <img src="../assets/logo.png" alt="Minha Figura" />
+      </figure>
+      <h3 class="card-header text-center">Area de Registro</h3>
+      <div class="form-row">
+        <div class="form-group col-md-6">
+          <div class="summary text-red">Form has errors</div>
+          <form @submit.prevent="register">
+            <div class="form-row">
+              <div class="form-group">
+                <aria-label for="name">Nome</aria-label>
+                <input
+                  placeholder="Informe um nome"
+                  v-model="v$.name.$model"
+                  class="form-control"
+                  type="text"
+                  name="name"
+                  id="name"
+                />
+                <span v-if="v$.name.$error" class="text-danger"
+                  >Campo é requerido</span
+                >
+              </div>
+            </div>
+            <div class="form-row">
+              <div class="form-group">
+                <aria-label for="password">Defina sua senha</aria-label>
+                <input
+                  placeholder="Senha forte requerida"
+                  class="form-control"
+                  type="password"
+                  name="password"
+                  id="password"
+                  v-model="v$.password.$model"
+                />
+                <span v-if="v$.password.$error" class="text-danger"
+                  >Letra maiúscula || Número || Caracter especial || Mínimo 8
+                  caracteres
+                </span>
+              </div>
+            </div>
+            <div class="form-row">
+              <div class="form-group">
+                <aria-label for="confirmation_pass"
+                  >Confirme sua senha</aria-label
+                >
+                <input
+                  placeholder="Repita a senha acima"
+                  class="form-control"
+                  type="password"
+                  name="confirm_password"
+                  id="confirm_password"
+                  v-model="v$.confirm_password.$model"
+                />
+                <span v-if="v$.confirm_password.$error" class="text-danger"
+                  >Necessário ser igual ao campo password</span
+                >
+              </div>
+            </div>
+            <div class="col">
+              <button type="submit" class="btn btn-secondary">Registrar</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import useVuelidate from "@vuelidate/core";
+import { required, minLength, sameAs } from "@vuelidate/validators";
+
+export default {
+  setup() {
+    return { v$: useVuelidate() };
+  },
+  data() {
+    return {
+      name: "",
+      password: "",
+      confirm_password: "",
+    };
+  },
+  methods: {
+    register() {
+      if (
+        !this.v$.name.$invalid &&
+        !this.v$.password.$invalid &&
+        !this.v$.confirm_password.$invalid
+      ) {
+        const user = JSON.stringify({
+          name: this.name,
+          password: this.password,
+        });
+        localStorage.user = user;
+      }
+    },
+  },
+  validations() {
+    return {
+      name: { required },
+      password: {
+        required,
+        minLength: minLength(8),
+        containsUppercase: function (value) {
+          return /[A-Z]/.test(value);
+        },
+        containsLowercase: function (value) {
+          return /[a-z]/.test(value);
+        },
+        containsNumber: function (value) {
+          return /[0-9]/.test(value);
+        },
+        containsSpecial: function (value) {
+          return /[#?!@$%^&*-]/.test(value);
+        },
+      },
+      confirm_password: { required, sameAsPassword: sameAs(this.password) },
+    };
+  },
+  components: {},
+};
+</script>
+<style>
+.card-header {
+  justify-content: center;
+  align-items: center;
+}
+.login {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 40%;
+}
+.card {
+  padding: 10%;
+}
+.btn-dark {
+  width: 100%;
+}
+.form-group {
+  width: 200px;
+  justify-content: center;
+  padding-bottom: 5%;
+  margin-top: 20px;
+}
+.form-control {
+  width: 250px;
+}
+.col {
+  margin-top: 20px;
+  margin-left: 50%;
+  width: 100%;
+}
+</style>
